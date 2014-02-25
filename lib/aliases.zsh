a19b91194143a41cfb86a26f3540c7f567ea322e
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-02-25 12:49:40 -0800

    Added aria2tor and brewin aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -32,8 +32,10 @@ alias hardin='ssh hardin'
 alias pingoo='ping -c 5 www.google.com'
 alias ....='../../..'
 alias update='brew -v update && brew upgrade'
+alias brewin='brew -v install'
 alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias irc='weechat-curses'
+alias aria2tor='aria2c --conf-path=${HOME}/.aria2/aria2.torrent'
 
 # Mac OS X application aliases for use in shell.
 alias firefox='open -a "Firefox"'
