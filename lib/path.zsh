eb1a5ca08539f538171e684dc15da79b7ecc60da
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-08-17 21:06:52 +1000

    Added GHCForMac installation to path
    - minor refactoring on old $HOME references

--- lib/path.zsh
+++ lib/path.zsh
@@ -1,7 +1,7 @@
 # Custom path additions.
 
 # Where my own scripts reside.
-USER_BIN=($HOME/Dropbox/bin $HOME/bin)
+USER_BIN=(${HOME}/Dropbox/bin ${HOME}/bin)
 
 # Where mactex puts its bin.
 LATEX_BIN=(/usr/local/texlive/2014/bin/universal-darwin)
@@ -12,6 +12,10 @@ HOMEBREW_BIN=(/usr/local/bin /usr/local/sbin)
 # Where OS X puts its bins.
 SYSTEM_BIN=(/bin /usr/bin /sbin /usr/sbin)
 
+# GHCForMac path
+GHC_VERSION=7.8.3
+GHC_BIN=(${HOME}/.cabal/bin /opt/homebrew-cask/Caskroom/ghc/${GHC_VERSION}-r1/ghc-${GHC_VERSION}.app/Contents/bin)
+
 # Actually set the path in Z shell.
 typeset -U path
-path=($USER_BIN $LATEX_BIN $HOMEBREW_BIN $SYSTEM_BIN)
+path=($USER_BIN $LATEX_BIN $HOMEBREW_BIN $SYSTEM_BIN $GHC_BIN)
